a7bbb025918c26ba64ec4e57ecaef70dce63bc8b
dbarneche <diego.barneche@ymail.com>
2013-07-24 16:22:50 +1000

    fixed dependencies - removed unecessary bits

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,9 +1,3 @@
-source("R/import.R")
-
 dataDir  <-  "datasets"
 FigsDir  <-  "figures"
 OutsDir  <-  "output"
-
-packages  <-  c("vegan", "bipartite")
-packages  <-  invisible(sapply(packages, isPack, verbose=TRUE))
-rm(packages)
